8bf85e8fbade391e3770834e46d465e501edbc56
santosh <santosh.ece06@gmail.com>
2019-01-29 13:38:19 +0530

    document access.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 
 function addNewBlock() {
     console.log("Add New Block", document);
+
+    let parent = document.querySelector(".container-fluid");
+    let ref = parent.children[0];
+    let newChild = ref.cloneNode(true);
+
+    parent.appendChild(newChild);
 }
 
 export {addNewBlock}
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ let path = require('path');
 
 module.exports = {
     "entry" : "./src/index.js",
+    "mode" : 'development',
 
     "output" : {
         path : path.resolve(__dirname, 'dist'),
