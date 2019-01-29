d544ce9a0496e15dbd97267b9daedbba24ed76db
santosh <santosh.ece06@gmail.com>
2019-01-29 13:02:54 +0530

    css loader.

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,11 @@ module.exports = {
     "output" : {
         path : path.resolve(__dirname, 'dist'),
         filename : 'bundle.js'
+    },
+
+    "module" : {
+        "rules" : [
+            {"test" : /\.css$/, "use": ['style-loader', 'css-loader']}
+        ]
     }
 }
